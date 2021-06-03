9f58326cf45f4fe4e0444ed8faa915c05db56c32
vcrl <vruel@pm.me>
2021-06-03 10:04:08 +0200

    final commit

--- GameManager.cs
+++ GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,13 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private bool gameHasEnded = false;
+    public bool hasWon = false;
+
+    private void Start()
+    {
+        GameObject.Find("Player").GetComponent<Animator>().SetBool("hasWon", hasWon);
+    }
+
     public void GameOver()
     {
         if (gameHasEnded == false)
@@ -23,10 +31,12 @@ public class GameManager : MonoBehaviour
     {
         if (gameHasEnded == false)
         {
+            hasWon = true;
             gameHasEnded = true;
             Debug.Log("Game over");
             GameObject.Find("GameOver/Win").GetComponent<DisplayGameOver>().DisplayGameOverText("YOU WIN");
             Invoke("StartMenu", 4f);
+            GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = false;
         }
     }
     void RestartGame()
--- PickupCollectibles.cs
+++ PickupCollectibles.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PickupCollectibles : MonoBehaviour
 {
     public Rigidbody rb;
+    public AudioSource soundFX;
     public int playerScore;
     public PlayerMovement playerMovement;
     
@@ -16,6 +17,7 @@ public class PickupCollectibles : MonoBehaviour
             Destroy(other.gameObject);
             playerScore += 1;
             playerMovement.maxSpeed += 1;
+            soundFX.Play();
         }
     }
 }
